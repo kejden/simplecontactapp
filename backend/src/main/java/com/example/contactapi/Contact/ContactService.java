33dddb0d6984dbc04c6228564ad9d4204d46300f
Gabriel Charkiewicz <108090061+kejden@users.noreply.github.com>
2024-05-26 23:33:12 +0200

    Contact class, Repository and Service

---
+++ backend/src/main/java/com/example/contactapi/Contact/ContactService.java
@@ -0,0 +1,75 @@
+package com.example.contactapi.Contact;
+
+import jakarta.transaction.Transactional;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort;
+import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.Optional;
+import java.util.function.BiFunction;
+import java.util.function.Function;
+
+import static com.example.contactapi.constant.Constant.PHOTO_DIR;
+import static java.nio.file.StandardCopyOption.REPLACE_EXISTING;
+
+@Service
+@Slf4j // TODO ADD LOGGING
+@Transactional(rollbackOn = Exception.class)
+@RequiredArgsConstructor
+public class ContactService {
+    private final ContactRepository contactRepository;
+
+    public Page<Contact> getAllContact(int page, int size){
+        return contactRepository.findAll(PageRequest.of(size,page,Sort.by("name")));
+    }
+
+    public Contact getContact(String id){
+        return contactRepository.findByID(id).orElseThrow(() -> new RuntimeException("Contact not found"));
+    }
+
+    public Contact createContact(Contact contact){
+        return contactRepository.save(contact);
+    }
+
+    public void deleteContact(Contact contact){
+        contactRepository.deleteById(contact.getId());
+    }
+
+    public String uploadPhoto(String id, MultipartFile file){
+        Contact contact = getContact(id);
+        String photoURL = photoFunction.apply(id, file);
+        contact.setPhotoURL(photoURL);
+        contactRepository.save(contact);
+        return photoURL;
+    }
+
+    private final Function<String, String> fileExtension = filename ->
+            Optional.of(filename)
+                    .filter(name -> name.contains("."))
+                    .map(name -> "." + name.substring(filename.lastIndexOf(".") + 1))
+                    .orElse(".png");
+
+    private final BiFunction<String, MultipartFile, String> photoFunction = (id, image) -> {
+        String filename = fileExtension.apply(image.getOriginalFilename());
+        try{
+            Path fileStorageLocation = Paths.get(PHOTO_DIR).toAbsolutePath().normalize();
+            if(!Files.exists(fileStorageLocation)){
+                Files.createDirectories(fileStorageLocation);
+            }
+            Files.copy(image.getInputStream(), fileStorageLocation.resolve(id + filename), REPLACE_EXISTING);
+            return ServletUriComponentsBuilder
+                    .fromCurrentContextPath()
+                    .path("/contacts/image/" + id +  filename).toUriString();
+        }catch(Exception e){
+            throw new RuntimeException("Unable to save image.");
+        }
+    }
+}
---
+++ backend/src/main/java/com/example/contactapi/constant/Constant.java
@@ -0,0 +1,5 @@
+package com.example.contactapi.constant;
+
+public class Constant {
+    public static final String PHOTO_DIR = System.getProperty("user.home") + "/Downloads/uploads";
+}
