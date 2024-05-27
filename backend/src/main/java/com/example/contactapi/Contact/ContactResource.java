9a479a73918fbe4c4012d9f6e63067ef59973eb8
Gabriel Charkiewicz <108090061+kejden@users.noreply.github.com>
2024-05-27 23:54:40 +0200

    Added deleteContact and getContact by id

--- backend/src/main/java/com/example/contactapi/Contact/ContactResource.java
+++ backend/src/main/java/com/example/contactapi/Contact/ContactResource.java
@@ -9,6 +9,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.net.URI;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/contacts")
@@ -16,15 +17,30 @@ import java.net.URI;
 public class ContactResource {
     private final ContactService contactService;
 
+    @PostMapping
+    public ResponseEntity<Contact> createContact(@RequestBody Contact contact){
+        return ResponseEntity.created(URI.create("/contacts/userID")).body(contactService.createContact(contact));
+    }
+
     @GetMapping()
     public ResponseEntity<Page<Contact>> getContacts(@RequestParam(value = "page", defaultValue="0") int page,
                                                      @RequestParam(value = "size", defaultValue = "10") int size){
         return ResponseEntity.ok().body(contactService.getAllContact(page, size));
     }
 
-    @PostMapping
-    public ResponseEntity<Contact> createContact(@RequestBody Contact contact){
-        return ResponseEntity.created(URI.create("/contacts/userID")).body(contactService.createContact(contact));
+    @GetMapping("/{id}")
+    public ResponseEntity<Contact> getContact(@PathVariable(value = "id") String id){
+        return ResponseEntity.ok().body(contactService.getContact(id));
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<String> deleteContact(@PathVariable(value = "id") String id){
+        Optional<Contact> contact = Optional.ofNullable(contactService.getContact(id));
+        if(contact.isPresent()){
+            contactService.deleteContact(contact.get());
+            return ResponseEntity.ok().build();
+        }
+        return ResponseEntity.notFound().build();
     }
 
 }
